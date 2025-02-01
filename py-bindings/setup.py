eb5a074d3257ced7e001bc7fb2c1677bfbb06f3b
Johnny <johnnynuca14@gmail.com>
2025-02-01 19:54:35 +0100

    first commit

--- py-bindings/setup.py
+++ py-bindings/setup.py
@@ -50,6 +50,7 @@ class CMakeBuild(build_ext):
         # EXAMPLE_VERSION_INFO shows you how to pass a value into the C++ code
         # from Python.
         cmake_args = [
+            f"-DCMAKE_CXX_STANDARD=14",
             f"-DCMAKE_CXX_COMPILER={shutil.which('clang++')}",  # Force Clang for castxml
             f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={extdir}{os.sep}",
             f"-DPYTHON_EXEC={sys.executable}",
