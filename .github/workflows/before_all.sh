181280a34e6231e2d5ff37e0c26df0186b0b2bee
Johnny <johnnync13@gmail.com>
2025-02-01 20:48:46 +0100

    Update before_all.sh

--- .github/workflows/before_all.sh
+++ .github/workflows/before_all.sh
@@ -5,11 +5,34 @@ set -eux
 build_os="$(uname)"
 
 if [ "${build_os}" == "Linux" ]; then
+    # Instalar dependencias con yum
     yum -y install \
-        sudo \
-        eigen3 \
-        llvm-devel \
-        clang-devel
+        gcc-c++ \
+        cmake \
+        pkgconfig \
+        boost-devel \
+        eigen3-devel \
+        ode-devel \
+        wget \
+        yaml-cpp-devel \
+        python3-devel \
+        python3-pip \
+        boost-python3-devel \
+        boost-numpy3-devel \
+        python3-numpy \
+        pypy3
+
+    # Instalar dependencias de la aplicación si se va a construir OMPL.app
+    # Adaptar la condición según cómo se determine si se construye OMPL.app
+    if [[ "${CIBW_BUILD}" == *"cp3"* ]]; then # Ejemplo de condición
+      yum -y install \
+          freeglut-devel \
+          assimp-devel \
+          python3-PyOpenGL \
+          python3-flask \
+          python3-celery \
+          libccd-devel
+    fi
 
     # manylinux ships with a pypy installation. Make it available on the $PATH
     # so the OMPL build process picks it up and can make use of it during the
